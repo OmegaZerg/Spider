735f3c42961d7f07fba8ca0fe464c0851d0511f4
OmegaZerg <Ragged17@Bellsouth.net>
2025-08-24 10:03:11 -0500

    Added 2 more functions for crawl.py, and also added several unit tests for each of those within the test_crawl.py file. Testing and troubleshooting, now everything is working as expected.

--- crawl.py
+++ crawl.py
@@ -1,7 +1,29 @@
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
 
-def normalize_url(url: str):
+def normalize_url(url: str) -> str:
     if url == "" or url == None:
         raise ValueError("The normalize url function must not be called with an empty string!")
     parsed_url = urlparse(url.lower())
     return parsed_url.hostname + parsed_url.path.rstrip("/") if parsed_url.hostname != None else parsed_url.path.rstrip("/")
+
+def get_h1_from_html(html: str) -> str:
+    soup = BeautifulSoup(html, 'html.parser')
+    if soup.h1 == None:
+        return ""
+    #print(f"Soup P: {soup.p}")
+    #print(f"Soup P Contents: {soup.p.string}")
+    #print(f"Soup h1: {soup.find('h1')}")
+    #print(f"Soup h1 full: {soup.h1}")
+    #print(f"Soup h1 contents: {soup.h1.string}")
+    return soup.h1.get_text(strip=True) if soup.h1.string != None else ""
+
+def get_first_paragraph_from_html(html: str) -> str:
+    soup = BeautifulSoup(html, 'html.parser')
+    paragraphs = soup.find_all('p')
+    if len(paragraphs) < 1:
+        return ""
+    main_html = soup.find("main")
+    if main_html is None:
+        return paragraphs[0].get_text()
+    return main_html.p.get_text(strip=True) if main_html.p.get_text() !="" else paragraphs[0].get_text()
--- test_crawl.py
+++ test_crawl.py
@@ -1,5 +1,5 @@
 import unittest
-from crawl import normalize_url
+from crawl import normalize_url, get_h1_from_html, get_first_paragraph_from_html
 
 class TestCrawl(unittest.TestCase):
     def test_normalize_url_1(self):
@@ -51,8 +51,90 @@ class TestCrawl(unittest.TestCase):
         except ValueError as e:
             assert str(e) == "The normalize url function must not be called with an empty string!"
             
+    def test_get_h1_from_html_1(self):
+        input_body = '<html><body><h1>Test Title</h1></body></html>'
+        actual = get_h1_from_html(input_body)
+        expected = "Test Title"
+        self.assertEqual(actual, expected)
 
+    def test_get_h1_from_html_2(self):
+        input_body = '<html><body><h1></h1></body></html>'
+        actual = get_h1_from_html(input_body)
+        expected = ""
+        self.assertEqual(actual, expected)
 
+    def test_get_h1_from_html_3(self):
+        input_body = '<html><body><p>Test paragraph</p></body></html>'
+        actual = get_h1_from_html(input_body)
+        expected = ""
+        self.assertEqual(actual, expected)
+
+    def test_get_h1_from_html_4(self):
+        input_body = '<html><body><h1>Test Title</h1><h1>A second h1!</h1></body></html>'
+        actual = get_h1_from_html(input_body)
+        expected = "Test Title"
+        self.assertEqual(actual, expected)
+
+    def test_get_h1_from_html_5(self):
+        input_body = '<html><body><h1>    Test Title Spaces    </h1></body></html>'
+        actual = get_h1_from_html(input_body)
+        expected = "Test Title Spaces"
+        self.assertEqual(actual, expected)
+
+    def test_get_first_paragraph_from_html_main_priority_1(self):
+        input_body = '''<html><body>
+            <p>Outside paragraph.</p>
+            <main>
+                <p>Main paragraph.</p>
+            </main>
+        </body></html>'''
+        actual = get_first_paragraph_from_html(input_body)
+        expected = "Main paragraph."
+        self.assertEqual(actual, expected)
+
+    def test_get_first_paragraph_from_html_main_priority_2(self):
+        input_body = '''<html><body>
+            <p>Outside paragraph.</p>
+            <main>
+                <p></p>
+            </main>
+        </body></html>'''
+        actual = get_first_paragraph_from_html(input_body)
+        expected = "Outside paragraph."
+        self.assertEqual(actual, expected)
+
+    def test_get_first_paragraph_from_html_main_priority_3(self):
+        input_body = '''<html><body>
+            <p></p>
+            <main>
+                <p></p>
+            </main>
+        </body></html>'''
+        actual = get_first_paragraph_from_html(input_body)
+        expected = ""
+        self.assertEqual(actual, expected)
+
+    def test_get_first_paragraph_from_html_main_priority_4(self):
+        input_body = '''<html><body>
+            <p>We want this to be returned!</p>
+            <stuff>
+                <p>This shouldnt be returned</p>
+            </stuff>
+        </body></html>'''
+        actual = get_first_paragraph_from_html(input_body)
+        expected = "We want this to be returned!"
+        self.assertEqual(actual, expected)
+
+    def test_get_first_paragraph_from_html_main_priority_5(self):
+        input_body = '''<html><body>
+            <h1>This is a header</h1>
+            <stuff>
+                <h2>This is a smaller header</h2>
+            </stuff>
+        </body></html>'''
+        actual = get_first_paragraph_from_html(input_body)
+        expected = ""
+        self.assertEqual(actual, expected)
 
 if __name__ == "__main__":
     unittest.main()
