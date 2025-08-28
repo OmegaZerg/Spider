c21d7ba32c5b3b17f05ded21986b97d115489d45
OmegaZerg <Ragged17@Bellsouth.net>
2025-08-28 06:49:35 -0500

    Updated main.py to look for an accept a comand line argument for the website url that we wish to crawl. Implemented very basic checks just to ensure we have 1 argument provided by the user, no more and no less. Possibly need to add some more robust checks to ensure this command line argument is a valid link.

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+import sys
+
 def main():
     print("Hello from spider!")
-
+    if len(sys.argv) < 2:
+        print("No website provided")
+        sys.exit(1)
+    if len(sys.argv) > 2:
+        print("Too many arguments provided")
+        sys.exit(1)
+    print(f"Starting crawl of: {sys.argv[1]}")
 
 if __name__ == "__main__":
     main()
