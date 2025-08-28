331dcddb8080da6305ce1ec6e4a7579f01c74fb3
OmegaZerg <Ragged17@Bellsouth.net>
2025-08-28 06:57:24 -0500

    Added check to command line argument to ensure the program is passed a full url that starts with either 'https://' or 'http://'.

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
     if len(sys.argv) > 2:
         print("Too many arguments provided")
         sys.exit(1)
+    if not sys.argv[1].startswith("https://") and not sys.argv[1].startswith("http://"):
+        print("Website provided must start with either 'https://' or 'http://'. Please provide the full link.")
+        sys.exit(1)
     print(f"Starting crawl of: {sys.argv[1]}")
 
 if __name__ == "__main__":
